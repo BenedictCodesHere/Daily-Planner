38b024a020db6366c0e3b13da5e0b7e0dede7948
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-01-28 17:36:15 +0000

    trying to solve the future boxes not staying green problem

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 let thisHour = moment().format('HH');
 console.log(thisHour);
+
+let parsedObject = {
+   9:"",
+   10:"",
+   11:"",
+   12:"",
+   13:"",
+   14:"",
+   15:"",
+   16:"",
+   17:""
+};
+
 let retrievedObject = localStorage.getItem('dataObject');
-let parsedObject = JSON.parse(retrievedObject);
-console.log(parsedObject);
+if (retrievedObject != null) {
+   parsedObject = JSON.parse(retrievedObject);
+}
+
 
 $('.hour').css({'justify-content': 'flex-end', 'align-items': 'center'});
 
@@ -10,7 +25,7 @@ $('.button-box').css('padding', '0px')
 $('.saveBtn').css({'width': '100%', 'height': '100%', 'border':'none', 'margin':'0', 'padding': '0'});
 $('i').css('font-size', '25px');
 
-for (let i = 9; i < 17; i++) {
+for (let i = 9; i < 20; i++) {
    $('.form-control').eq(i - 9).text(parsedObject[i])
    $('.time-block').eq(0).clone().appendTo('#time-block-container');
    var newBlock = $('.time-block').last()
@@ -27,25 +42,59 @@ for (let i = 9; i < 17; i++) {
 
 }
 
-var filteredHour = $('.time-block').filter(function(){
+var presentHour = $('.time-block').filter(function(){
    return $(this).data("number") == thisHour;
 })
 
-var presentBox = filteredHour.children().eq(1);
+var futureHour = $('.time-block').filter(function(){
+   return $(this).data("number") > thisHour;
+})
+console.log(futureHour);
+
+for (let index = 0; index < futureHour.length; index++) {
+
+   var misanthrope = futureHour[index];
+
+   
+   var futureBoxes = $(misanthrope).children().eq(1)
+
+   console.log(futureBoxes)
+   futureBoxes.addClass('future')
+   futureBoxes.children().addClass('future');
+   //misanthrope.children().eq(1);
+   //.children().eq(1).addClass('future');
+  // futureBox.children().addClass('future');
+}
+
+var presentBox = presentHour.children().eq(1);
 
 presentBox.addClass('present');
 presentBox.children().addClass('present');
 
 
-console.log(filteredHour);
+console.log(presentHour);
 
+console.log(presentBox.children())
 
 
 $('.form-outline').on('click', function(event){
     event.preventDefault();
-    $(this).find('textarea').focus().css({'color':'white', 'background-color':'#d3d3d3'});
-    console.log('clicked')
-    }
+    console.log(event.target);
+    console.log(presentBox[0]);
+    console.log(presentBox.children()[0]);
+    if(event.target == presentBox[0] || event.target == presentBox.children()[0]) {
+      console.log('just clicked me')
+      $(this).find('textarea').focus();
+      $(":focus").css('background-color', '#ff6961');
+   } else if(event.target == futureBoxes || event.target == futureBoxes.children()[0]) {
+      console.log('future clickage')
+   } else {
+      $(this).find('textarea').focus().css({'color':'white', 'background-color':'#d3d3d3'});
+      console.log('clicked')
+   }
+   
+   
+   }
 );
 
 
