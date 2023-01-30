921a631a42ff45d03031e0b5f3d7a4fc161c1086
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-01-30 03:05:23 +0000

    fixed all bugs so the application works as planned

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-let momentumag = (moment().format('HH'));
-let thisHour = Number(momentumag);
+let stringHour = (moment().format('HH'));
+let thisHour = Number(11);
 console.log(thisHour);
 console.log(typeof thisHour)
 let now = moment();
@@ -8,8 +8,7 @@ function setTime() {
    now = moment();
    $('#currentDay').text(now);
 }
-setInterval(setTime, 1000)
-
+setInterval(setTime, 1000);
 
 let parsedObject = {
    9:"",
@@ -26,14 +25,14 @@ let parsedObject = {
 let retrievedObject = localStorage.getItem('dataObject');
 if (retrievedObject != null) {
    parsedObject = JSON.parse(retrievedObject);
-}
+};
 
 console.log(parsedObject);
 
 
 $('.hour').css({'justify-content': 'flex-end', 'align-items': 'center'});
 
-$('.button-box').css('padding', '0px')
+$('.button-box').css('padding', '0px');
 $('.saveBtn').css({'width': '100%', 'height': '100%', 'border':'none', 'margin':'0', 'padding': '0'});
 $('i').css('font-size', '25px');
 
@@ -41,23 +40,16 @@ for (let i = 9; i < 17; i++) {
    console.log($('.form-control'));
    $('.form-control').eq(i - 9).text(parsedObject[i]);
    $('.time-block').eq(0).clone().appendTo('#time-block-container');
-   var newBlock = $('.time-block').last()
+   $('.time-block').first().addClass('past');
+   var newBlock = $('.time-block').last();
    newBlock.attr('data-number', i+1);
    newBlock.children().first().text(`${i + 1}:00`)
+   newBlock.children().eq(1).addClass('past');
 
    if (i == 16) {
       $('.form-control').eq(8).text(parsedObject[17]);
-   }
-
-   
-   // newBlock.children().eq(1).children().text(parsedObject[i+1])
-   // console.log(newBlock.children().first().text)
-   // console.log('Newblock.first is:' + newBlock.first());
-   // console.log($('.time-block'));
-   // console.log($(newBlock).attr('data-number'));
-   //newBlock.first().text(i)
-
-}
+   };
+};
 
 var presentHour = $('.time-block').filter(function(){
    return $(this).data("number") == thisHour;
@@ -68,7 +60,7 @@ console.log(presentHour);
 //    if(Number($('.time-block').data('number')) > thisHour){
 //       return $('.time-block').data("number");
 //    }
-   
+
 // })
 var futureHour = $('.time-block').filter(function(){
    return $(this).data("number") > thisHour;
@@ -81,7 +73,7 @@ for (let index = 0; index < futureHour.length; index++) {
 
    var currentBox = futureHour[index];
 
-   
+
    var futureBox = $(currentBox).children().eq(1);
    futureBox.removeClass('past');
    futureBox.children().eq(0).removeClass('past');
@@ -102,17 +94,10 @@ console.log(presentHour);
 console.log(presentBox.children())
 
 
-// if event.target IS IN A ROW THAT HAS A VALUE GREATER THAN THISHOUR THEN
-// MAKE SURE THAT WHATEVER THE EVENT TARGET IS, IS STYLED ACCORDING TO THE FUTURE
-// STYLINGS (STAYS GREEN)
-
-
 $('.form-outline').on('mousedown', function(event){
-    // event.preventDefault();
     console.log(event.target);
     var brutal = event.target;
     console.log(brutal);
-    
     if(event.target == presentBox[0] || event.target == presentBox.children()[0]) {
       console.log('just clicked me')
       $(this).find('textarea').focus();
@@ -122,40 +107,32 @@ $('.form-outline').on('mousedown', function(event){
       $(this).find('textarea').focus();
       $(":focus").css('background-color', '#77dd77');
    } else {
-      $(this).find('textarea').focus().css({'color':'white', 'background-color':'black'});
+      $(this).find('textarea').focus().css('background-color','#d3d3d3');
       console.log('clicked');
    }
-   
-   
+
+
    }
 );
 
-
 const dataObject = parsedObject;
-
-
-
-
-
 
 
 $('.saveBtn').on('click', function (event) {
    var clickedNumber = $(this).parent().parent().attr('data-number');
    console.log(clickedNumber);
-   var textInput = $(this).parent().prev().children().val()
+   var textInput = $(this).parent().prev().children().val();
 
 
    let assignData = function(dataObjectParam, messageParam, clickedNumberParam) {
-      
+
       for (const property in dataObjectParam) {
-      // if(message == dataObjectParam[property])
          if(property == clickedNumberParam) {
             dataObjectParam[property] = messageParam;
             console.log(`Here it is lads -  ${property}: ${dataObjectParam[property]}`);
-         }
-   
-         }
-      }
+          };
+       };
+   };
 
       assignData(dataObject, textInput, clickedNumber);
       console.log(dataObject);
@@ -163,15 +140,6 @@ $('.saveBtn').on('click', function (event) {
       retrievedObject = localStorage.getItem('dataObject');
       parsedObject = JSON.parse(retrievedObject);
       console.log(parsedObject);
-    
-//    let SaveData = function(message, hour) {
-//     this.message = message;
-//     this.hour = hour;
-//    }
-//    console.log(textInput);
-//    let freshSaveData = new SaveData(textInput, clickedNumber);
-//    console.log(freshSaveData)
-//    localStorage.setItem('dataObject', JSON.stringify(dataObject))
 
 });
 
